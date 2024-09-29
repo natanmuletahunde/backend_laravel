90753e430e5533d53ba5d32c4317876c4c9e76a8
pivnati <natanmuleta911@gmail.com>
2024-09-28 23:05:52 -0700

    create the migration table

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    // Specify the table name (optional if the table follows Laravel naming conventions)
+    protected $table = 'todos';
+
+    // Specify which fields can be mass-assigned
+    protected $fillable = ['task'];
 }
