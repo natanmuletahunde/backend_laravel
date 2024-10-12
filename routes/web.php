d34cde5c872bd64efb7de9eea69ccf50f1cec31d
pivnati <natanmuleta911@gmail.com>
2024-10-11 18:19:35 -0700

    create the api.php in the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,5 +10,8 @@ Route::get('/hello',function(){
     return response('<h1>hello piv nati u do great thing</h1>');
 });
 Route::get('/posts/{id}',function($id){
-       return response('post  '. $id);
+      return response('post  '. $id);
 })->where('id','[0-9]+');// this make the post to find only numbers
+Route::get('/search',function(Request $request){
+      return $request->name. ' ' . $request->city;
+});
