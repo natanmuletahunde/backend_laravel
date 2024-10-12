806c87395a67a278b0f32f97b1d0462ac95e8686
pivnati <natanmuleta911@gmail.com>
2024-10-11 17:57:09 -0700

    make the route to get the page from the itself

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello',function(){
+    return response('<h1>hello piv nati u do great thing</h1>');
+});
+Route::get('/posts/{id}',function($id){
+       return response('post  '. $id);
+})->where('id','[0-9]+');// this make the post to find only numbers
